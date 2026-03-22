c03e9a3b587471a2558e3c7a389ebfa787230284
Ivan Olshnitsky <i.olshnitskii@start.ru>
2026-03-22 14:27:27 +0300

    iter25

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,20 +1,39 @@
 package config
 
 import (
+	"encoding/json"
 	"flag"
 	"os"
 )
 
 type AppConfig struct {
-	ServerAddress   string
-	BaseURL         string
-	LogLevel        string
-	FileStoragePath string
-	SecretKey       string
-	DSN             string
-	AuditFile       string
-	AuditURL        string
-	EnableHTTPS     bool
+	ServerAddress   string `json:"server_address"`
+	BaseURL         string `json:"base_url"`
+	LogLevel        string `json:"log_level"`
+	FileStoragePath string `json:"file_storage_path"`
+	SecretKey       string `json:"secret_key"`
+	DSN             string `json:"database_dsn"`
+	AuditFile       string `json:"audit_file"`
+	AuditURL        string `json:"audit_url"`
+	EnableHTTPS     bool   `json:"enable_https"`
+}
+
+func loadFromFile(path string) (*AppConfig, error) {
+	if path == "" {
+		return &AppConfig{}, nil
+	}
+
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg AppConfig
+	if err := json.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
 }
 
 func NewAppConfig() *AppConfig {
@@ -27,6 +46,8 @@ func NewAppConfig() *AppConfig {
 	)
 
 	fs := flag.NewFlagSet("shortener", flag.ContinueOnError)
+
+	// flags
 	flagServer := fs.String("a", "", "server address")
 	flagBase := fs.String("b", "", "base url")
 	flagLogLevel := fs.String("l", "", "log level")
@@ -36,71 +57,128 @@ func NewAppConfig() *AppConfig {
 	flagDatabaseDSN := fs.String("d", "", "database DSN")
 	flagHTTPS := fs.Bool("s", false, "enable HTTPS")
 
+	var configPath string
+	fs.StringVar(&configPath, "c", "", "config file path")
+	fs.StringVar(&configPath, "config", "", "config file path")
+
 	_ = fs.Parse(os.Args[1:])
+
+	// ENV для config
+	if envConfig := os.Getenv("CONFIG"); envConfig != "" {
+		configPath = envConfig
+	}
+
+	// загрузка файла
+	fileCfg, _ := loadFromFile(configPath)
 
 	cfg := &AppConfig{}
 
+	// ========================
+	// ServerAddress
+	cfg.ServerAddress = defaultServerAddress
+	if fileCfg.ServerAddress != "" {
+		cfg.ServerAddress = fileCfg.ServerAddress
+	}
+	if *flagServer != "" {
+		cfg.ServerAddress = *flagServer
+	}
 	if val, ok := os.LookupEnv("SERVER_ADDRESS"); ok {
 		cfg.ServerAddress = val
-	} else if *flagServer != "" {
-		cfg.ServerAddress = *flagServer
-	} else {
-		cfg.ServerAddress = defaultServerAddress
 	}
 
+	// ========================
+	// BaseURL
+	cfg.BaseURL = defaultBaseURL
+	if fileCfg.BaseURL != "" {
+		cfg.BaseURL = fileCfg.BaseURL
+	}
+	if *flagBase != "" {
+		cfg.BaseURL = *flagBase
+	}
 	if val, ok := os.LookupEnv("BASE_URL"); ok {
 		cfg.BaseURL = val
-	} else if *flagBase != "" {
-		cfg.BaseURL = *flagBase
-	} else {
-		cfg.BaseURL = defaultBaseURL
 	}
 
+	// ========================
+	// LogLevel
+	cfg.LogLevel = defaultLogLevel
+	if fileCfg.LogLevel != "" {
+		cfg.LogLevel = fileCfg.LogLevel
+	}
+	if *flagLogLevel != "" {
+		cfg.LogLevel = *flagLogLevel
+	}
 	if val, ok := os.LookupEnv("LOG_LEVEL"); ok {
 		cfg.LogLevel = val
-	} else if *flagLogLevel != "" {
-		cfg.LogLevel = *flagLogLevel
-	} else {
-		cfg.LogLevel = defaultLogLevel
 	}
 
+	// ========================
+	// FileStoragePath
+	cfg.FileStoragePath = defaultFileStoragePath
+	if fileCfg.FileStoragePath != "" {
+		cfg.FileStoragePath = fileCfg.FileStoragePath
+	}
+	if *flagFileStoragePath != "" {
+		cfg.FileStoragePath = *flagFileStoragePath
+	}
 	if val, ok := os.LookupEnv("FILE_STORAGE_PATH"); ok {
 		cfg.FileStoragePath = val
-	} else if *flagFileStoragePath != "" {
-		cfg.FileStoragePath = *flagFileStoragePath
-	} else {
-		cfg.FileStoragePath = defaultFileStoragePath
 	}
+
+	// ========================
+	// SecretKey (без файла)
 	if val, ok := os.LookupEnv("SECRET_KEY"); ok {
 		cfg.SecretKey = val
 	} else {
 		cfg.SecretKey = defaultSecretKey
 	}
 
+	// ========================
+	// Database DSN
+	if fileCfg.DSN != "" {
+		cfg.DSN = fileCfg.DSN
+	}
+	if *flagDatabaseDSN != "" {
+		cfg.DSN = *flagDatabaseDSN
+	}
 	if val, ok := os.LookupEnv("DATABASE_DSN"); ok {
 		cfg.DSN = val
-	} else if *flagDatabaseDSN != "" {
-		cfg.DSN = *flagDatabaseDSN
-	} else {
-		cfg.DSN = ""
 	}
 
-	if val, ok := os.LookupEnv("AUDIT_FILE"); ok {
-		cfg.AuditFile = val
-	} else {
+	// ========================
+	// AuditFile
+	if fileCfg.AuditFile != "" {
+		cfg.AuditFile = fileCfg.AuditFile
+	}
+	if *flagAuditFile != "" {
 		cfg.AuditFile = *flagAuditFile
 	}
-
-	if val, ok := os.LookupEnv("AUDIT_URL"); ok {
-		cfg.AuditURL = val
-	} else {
-		cfg.AuditURL = *flagAuditURL
+	if val, ok := os.LookupEnv("AUDIT_FILE"); ok {
+		cfg.AuditFile = val
 	}
 
+	// ========================
+	// AuditURL
+	if fileCfg.AuditURL != "" {
+		cfg.AuditURL = fileCfg.AuditURL
+	}
+	if *flagAuditURL != "" {
+		cfg.AuditURL = *flagAuditURL
+	}
+	if val, ok := os.LookupEnv("AUDIT_URL"); ok {
+		cfg.AuditURL = val
+	}
+
+	// ========================
+	// EnableHTTPS
+	if fileCfg.EnableHTTPS {
+		cfg.EnableHTTPS = true
+	}
+	if *flagHTTPS {
+		cfg.EnableHTTPS = true
+	}
 	if val, ok := os.LookupEnv("ENABLE_HTTPS"); ok && val == "true" {
 		cfg.EnableHTTPS = true
-	} else {
-		cfg.EnableHTTPS = *flagHTTPS
 	}
 
 	return cfg
